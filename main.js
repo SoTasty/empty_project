d00c7b1d55ed85394ea500cd21db24438dab7cb5
SoTasty <turboslfi@gmail.com>
2022-02-03 19:14:08 +0600

    something

---
+++ main.js
@@ -0,0 +1,11 @@
+let button = document.getElementById("finnish")
+
+button.addEventListener("click", checkTest)
+
+function checkTest() {
+    alert(123)
+
+    let a1 = document.getElementById("q1").value
+    alert(a1)
+}
+
